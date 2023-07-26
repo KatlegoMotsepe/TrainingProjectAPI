6685cdeda7e145271569c405a8accce0c141a465
Katlego Motsepe <122864336+KatlegoMotsepe@users.noreply.github.com>
2023-07-26 15:11:44 +0200

    1st push

---
+++ TrainingProjectAPI/TrainingProjectAPI/DTOs/AddUserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace TrainingProjectAPI.DTOs
+{
+    public class AddUserDTO
+    {
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Surname { get; set; }
+
+        public string Password { get; set; }    
+    }
+}
---
+++ TrainingProjectAPI/TrainingProjectAPI/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using TrainingProjectAPI.DTOs;
+
+namespace TrainingProjectAPI.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Surname { get; set; } 
+        public byte[] PasswordHash { get; set; }    
+        public byte[] PasswordSalt { get; set; }    
+
+        public User() { }
+
+        public User(AddUserDTO userDTO)
+        {
+            UserId = Guid.NewGuid();    
+            Name = userDTO.Name;    
+            Surname = userDTO.Surname;
+            Email = userDTO.Email;
+            Hash_password(userDTO.Password);
+        }
+        public void Hash_password(string password)
+        {
+            using var BigMac = new HMACSHA512();
+
+            PasswordHash = BigMac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            PasswordSalt = BigMac.Key;
+
+        }
+    }
+}
